3370d564f136306b0a82307a3aa6c360789297e6
zhangpc <zhangpeichuan@126.com>
2020-12-09 16:50:33 +0800

    误删printf.c
    标准输出函数例子需重写

---
+++ C语言基础/io/printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("hello word!\n");
+    int a = 567;
+    printf("十进制 %d \n",a);
+    printf("十六进制无符号整形%X \n",a);
+    printf("八进制无符号整数%o \n",a);
+    printf("不带符号十进制整数%u \n",a);
+    printf("单一字符%c \n",a);
+    printf("字符串%s \n","ABC");
+
+
+
+    return 0;
+}
