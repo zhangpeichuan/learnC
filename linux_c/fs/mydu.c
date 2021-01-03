e444702b2ad98b39935e6100c1e319dee8058905
zhangpc <zhangpeichuan@126.com>
2021-01-03 18:10:41 +0800

    chkpass and timelog

--- linux_c/fs/mydu.c
+++ linux_c/fs/mydu.c
@@ -15,8 +15,9 @@ static int path_noloop(const char *path){
         return 0;
     return 1;
 }
+
 static int64_t mydu(const char *path){
-    struct  stat stat_res;
+    static struct  stat stat_res;
     static char    nextpath[PATHSIZE];
     glob_t  g;
     int64_t sum;
@@ -25,7 +26,6 @@ static int64_t mydu(const char *path){
         exit(1);
     }
     if(!S_ISDIR(stat_res.st_mode)){
-        printf("%lld    %s\n",stat_res.st_blocks,path);
         return stat_res.st_blocks;
     }
 
@@ -36,12 +36,14 @@ static int64_t mydu(const char *path){
     strncat(nextpath, "/.*", PATHSIZE);
     glob(nextpath, GLOB_APPEND, NULL, &g);
 
-    sum += stat_res.st_blocks;
+    sum = 0;
     for (int i = 0; i < g.gl_pathc; i++)
     {
         if (path_noloop(g.gl_pathv[i]))
             sum += mydu(g.gl_pathv[i]);
     }
+	sum +=stat_res.st_blocks;
+    printf("%lld	%s\n",sum/2,path);
     globfree(&g);
     return sum;
 }
@@ -52,7 +54,8 @@ int main(int argc ,char **argv){
         fprintf(stderr,"Usage ...");
         exit(1);
     }
-    printf("%lld    %s\n",mydu(argv[1]),argv[1]);
+	//mydu(argv[1]);
+    printf("%lld	%s\n",mydu(argv[1])/2,argv[1]);
 
 
     return 0;
